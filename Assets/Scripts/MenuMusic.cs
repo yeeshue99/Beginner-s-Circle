789a3d0f533009fa5aab1828d3b51d393b494688
yeeshue99 <yeeshue99@gmail.com>
2020-10-22 19:39:22 -0700

    waitign on menu muisc

---
+++ Assets/Scripts/MenuMusic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMusic : MonoBehaviour
+{
+    public AudioSource withoutTail;
+    public AudioSource withTail;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PlayMusic());
+    }
+
+    // Update is called once per frame
+    IEnumerator PlayMusic()
+    {
+        withoutTail.Play();
+        yield return new WaitForFixedUpdate();
+        withTail.Play();
+    }
+}
